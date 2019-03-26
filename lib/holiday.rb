b91f813a5f9e3726703fde6a7f754a9004f69023
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-26 20:29:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,6 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  
+holiday_hash[season][holiday].map {|x| x == "BBQ" }
+return contacts
 
 end
