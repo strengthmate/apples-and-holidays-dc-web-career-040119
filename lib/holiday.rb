7411e970eafaf8b796253916d8208ede3845e302
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-26 20:27:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
     
     holiday_hash.each do |season, data|
       puts "#{season.capitalize}:"
-      holidays.each do |holiday, supplies|
+      data.each do |holiday, supplies|
         puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
         end
       end
