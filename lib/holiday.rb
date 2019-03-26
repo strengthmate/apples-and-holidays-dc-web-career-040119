b64369f78a4e3a75842b27ff86b0f2313658ca61
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-26 20:26:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,14 +69,13 @@ def all_supplies_in_holidays(holiday_hash)
     # holiday_hash.each do |season, holiday, supply|
     # puts "#{season.capitalize}: #{supply}: #{supply}"
     
-
-  holiday_hash.each do |season, holidays|
-    puts "#{season.capitalize}:"
-    holidays.each do |holiday, supplies|
-      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    holiday_hash.each do |season, data|
+      puts "#{season.capitalize}:"
+      holidays.each do |holiday, supplies|
+        puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+        end
+      end
     end
-  end
-end
   
 
 def all_holidays_with_bbq(holiday_hash)
